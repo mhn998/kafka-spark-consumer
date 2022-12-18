827471c63e146618e1a8930f56d5d3bb714e88a7
Muhannad Mughrabi <mughrabi@Muhannads-MacBook-Pro.local>
2022-12-18 02:16:02 -0600

    fix hive script

--- hive/hive-dump.sh
+++ hive/hive-dump.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
-
 clean=false
-
-while getopts 'c' opt; do
-	case $opt in
-		c) clean=true;;
-		*) echo "Error unknown arg!" exit 1
-	esac
+while getopts ':c:h:' opt; do
+    case $opt in
+        c) clean=true;;
+        h) echo "Usage: ./hive-dump.sh [-c 1 | -h 1]"; exit;;
+        *) echo "Error unknown arg!" exit 1
+    esac
 done
-
-
 echo configuring hive ...
 if "$clean";
-	then if [[ $(echo "show tables like 'tweets'" | hive | grep 'tweets') ]];
-		then echo "deleting tweets table"
-		hive -e "drop table tweets"
-		hive -f ../confg/hive.hql;
-	else
-		hive -f ../confg/hive.hql;
-	fi
+    then if [[ $(echo "show tables like 'tweets'" | hive | grep 'tweets') ]];
+        then echo "deleting tweets table"
+        hive -e "drop table tweets"
+        hive -f hive.hql;
+    else
+        echo "clean else"
+        hive -f hive.hql;
+    fi
 else
-	hive -f ../confg/hive.hql;
+    echo "else"
+    hive -f hive.hql;
 fi
-
-
-
-
-
-
